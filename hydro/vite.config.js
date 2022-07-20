6411781fc8a4cc7357df0747ebc1bd9d052e6e11
anil843324 <anilkrsingh14200@gmail.com>
2022-07-20 13:02:08 +0530

    config for vite changed
    '

--- hydro/vite.config.js
+++ hydro/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// no chnges by developer
 export default defineConfig({
   plugins: [react()]
 })
