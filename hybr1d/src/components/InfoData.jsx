6b5684ce6b4959bbaa8db34dce91c8e0fe40b70b
anil843324 <anilkrsingh14200@gmail.com>
2022-07-20 08:06:54 +0530

    infoPage

--- hybr1d/src/components/InfoData.jsx
+++ hybr1d/src/components/InfoData.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import "./InfoData.css"
-import { useParams } from "react-router-dom"
 const InfoData = () => {
+  // get id params
+  const { id } = useParams();
 
-    const parmas = useParams();
+  const [objData, setObjData] = useState([]);
 
-     const objId=parmas
+  useEffect(() => {
 
+    const  fetchApi = async () => {
+    
+      const response = await fetch(`https://hn.algolia.com/api/v1/items/${id}`);
 
+      const datas = await response.json();
 
-    console.log(objId)
+      setObjData(datas.children);
+      
+    };
+      fetchApi().catch((error) => {
+     
+         console.log(error) 
+      
+    });
+  }, [id]);
 
-    return (
-        <div>InfoData</div>
-    )
-}
+  console.log(objData);
+  console.log(id);
+  return (
+    
+        <div className='mainContainer'>
+          { objData.length!==0 && objData.slice(0, 15).map((ele) => {
+           
+            return (
+              
+                <div key={ele.id} className="childContainer">
+                <h1 >Item : {ele.author}</h1>
+                <h3 >points: {ele.text}</h3>
+              
+                </div>
+            );
+          })}
+          
+        </div>
+  )
+};
 
-export default InfoData
+export default InfoData;
