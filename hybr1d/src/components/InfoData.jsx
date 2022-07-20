9d0b8a5ffc1b16e573b3e9c51b39497a678491d8
anil843324 <anilkrsingh14200@gmail.com>
2022-07-20 09:38:41 +0530

    some fetures added

--- hybr1d/src/components/InfoData.jsx
+++ hybr1d/src/components/InfoData.jsx
@@ -1,50 +1,62 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import HashLoader from "react-spinners/HashLoader";
 import "./InfoData.css"
+
+
 const InfoData = () => {
-  // get id params
-  const { id } = useParams();
+    // get id params
+    const { id } = useParams();
+    const [loading, setLoading] = React.useState(true);
+    const [objData, setObjData] = useState([]);
+    let [color, setColor] = useState("#36D7B7");
 
-  const [objData, setObjData] = useState([]);
 
-  useEffect(() => {
+    useEffect(() => {
 
-    const  fetchApi = async () => {
-    
-      const response = await fetch(`https://hn.algolia.com/api/v1/items/${id}`);
+        const fetchApi = async () => {
 
-      const datas = await response.json();
+            const response = await fetch(`https://hn.algolia.com/api/v1/items/${id}`);
 
-      setObjData(datas.children);
-      
-    };
-      fetchApi().catch((error) => {
-     
-         console.log(error) 
-      
-    });
-  }, [id]);
+            const datas = await response.json();
 
-  console.log(objData);
-  console.log(id);
-  return (
-    
-        <div className='mainContainer'>
-          { objData.length!==0 && objData.slice(0, 15).map((ele) => {
-           
-            return (
-              
-                <div key={ele.id} className="childContainer">
-                <h1 >Item : {ele.author}</h1>
-                <h3 >points: {ele.text}</h3>
-              
-                </div>
-            );
-          })}
-          
-        </div>
-  )
+            setObjData(datas.children);
+            setLoading(!loading)
+        };
+        fetchApi().catch((error) => {
+
+            console.log(error)
+            setLoading(!loading)
+
+        });
+    }, [id]);
+
+    console.log(objData);
+    console.log(id);
+    return (
+        <>
+
+
+            {loading ? <HashLoader color={color} loading={loading} className="loadingSpinner" size={70} /> : <div className='mainContainer'>
+                {objData.length !== 0 && objData.slice(0, 20).map((ele) => {
+
+                    return (
+
+                        <div key={ele.id} className="childContainer">
+                            <h2 >{ele.author}</h2>
+                            <h3 >{ele.text}</h3>
+
+                        </div>
+                    );
+                })}
+
+            </div>}
+
+
+
+        </>
+    )
 };
 
 export default InfoData;
