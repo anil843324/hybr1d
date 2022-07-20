0e74c4dc3ec074759c4abb320a3eb54a08af1f00
anil843324 <anilkrsingh14200@gmail.com>
2022-07-21 00:27:08 +0530

    some remains error

--- hydro/src/components/InfoData.jsx
+++ hydro/src/components/InfoData.jsx
@@ -1,65 +1,64 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 import HashLoader from "react-spinners/HashLoader";
 
-import "./InfoData.css"
-
+import "./InfoData.css";
 
 const InfoData = () => {
-    // get id params
-    const { id } = useParams();
-    const [loading, setLoading] = React.useState(true);
-    const [objData, setObjData] = useState([]);
-    let [color, setColor] = useState("#36D7B7");
+  // get id params
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(true);
+  const [objData, setObjData] = useState([]);
+  let [color, setColor] = useState("#36D7B7");
 
+  useEffect(() => {
+    const fetchApi = async () => {
+      const response = await fetch(`https://hn.algolia.com/api/v1/items/${id}`);
 
-    useEffect(() => {
+      const datas = await response.json();
 
-        const fetchApi = async () => {
+      setObjData(datas.children);
+      setLoading(!loading);
+    };
+    fetchApi().catch((error) => {
+      console.log(error);
+      setLoading(!loading);
+    });
+  }, [id]);
 
-            const response = await fetch(`https://hn.algolia.com/api/v1/items/${id}`);
-
-            const datas = await response.json();
-
-            setObjData(datas.children);
-            setLoading(!loading)
-        };
-        fetchApi().catch((error) => {
-
-            console.log(error)
-            setLoading(!loading)
-
-        });
-    }, [id]);
-
-    console.log(objData);
-    console.log(id);
-    return (
-        <>
-           
-
-
-            {loading ? <HashLoader color={color} loading={loading} className="loadingSpinner" size={70} /> : <div className='mainContainer'>
-                {objData.length !== 0 && objData.slice(0, 20).map((ele) => {
-
-                    return (
-
-                        <div key={ele.id} className="childContainer">
-                            <h2 >Title: {ele.title}</h2>
-                             <h2>Points: {ele.points}</h2>
-                            <div dangerouslySetInnerHTML={{ __html: ele.text }} />
-                        </div>
-                    );
-                })}
-
-            </div>}
-
-
-
-        </>
-    )
+  console.log(objData);
+  console.log(id);
+  return (
+    <>
+      {loading ? (
+        <HashLoader
+          color={color}
+          loading={loading}
+          className="loadingSpinner"
+          size={70}
+        />
+      ) : (
+        <div className="mainContainer">
+          {objData.length !== 0 &&
+            objData.slice(0, 20).map((ele) => {
+              return (
+                <div>
+                  {ele.text && (
+                    <div key={ele.id} className="childContainer">
+                      <h2>Title: {ele.title}</h2>
+                      <h2>Points: {ele.points}</h2>
+                      <div dangerouslySetInnerHTML={{ __html: ele.text }} />
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </>
+  );
 };
 
-export {InfoData};
+export { InfoData };
--- hydro/src/pages/Home.jsx
+++ hydro/src/pages/Home.jsx
@@ -1,87 +1,90 @@
-import React, { useEffect, useState ,useContext } from 'react'
-import "./Home.css"
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
 import SyncLoader from "react-spinners/SyncLoader";
-import { DataContext } from '../context/DataContext';
+import { DataContext } from "../context/DataContext";
 //<MoonLoader />
 const Home = () => {
+  const [queryData, setQueryData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const [queryData, setQueryData] = useState([]);
-    const [loading, setLoading] = React.useState(false);
-    
-    const {inputData} = useContext(DataContext)
-    let [color, setColor] = useState("#36D7B7");
-    
+  const { inputData } = useContext(DataContext);
+  const [color, setColor] = useState("#36D7B7");
+  const [isError,setIsError]=useState(false)
 
-
-    const queryUser = async () => {
-
-         
-        try {
-            setLoading(true)
-            const response = await fetch(
-                `https://hn.algolia.com/api/v1/search?query=${inputData}`
-            );
-            const data = await response.json();
-            setQueryData(data.hits);
-            setLoading(false)
-        } catch (error) {
-            console.log("anil erro",error)
-           
-        }
-    };
-
-    useEffect(() => {
-
-        let timeout = setTimeout(() => {
-            queryUser()
-            console.log(queryData)
-        }, 500)
-
-        return () => clearTimeout(timeout)
-
-    }, [inputData])
-
-
-
-
-
-    return (
-        <>
-          {/* <SearchData/> */}
+  const queryUser = async () => {
+    try {
+      setLoading(true);
       
-            {loading ? <SyncLoader color={color}  className="loadingSpinner" size={20} /> : <div className='searchContienr' >
-                { queryData.length>0 && queryData.map((news) => {
-                    return (
-                        <>
-                        { news.title &&
-                        <div key={news.objectID}
-                        onClick={()=> handleDiv}
+      const response = await fetch(
+        `https://hn.algolia.com/api/v1/search?query=${inputData}`
+      );
+      const data = await response.json();
+      setQueryData(data.hits);
+      setLoading(false);
+    } catch (error) {
+      console.log("anil erro", error);
+      setIsError(true)
+    
+    }
+  };
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      queryUser();
+      console.log(queryData);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [inputData]);
+
+  return (
+    <>
+      {/* <SearchData/> */}
+
+      {loading ? (
+        <SyncLoader color={color} className="loadingSpinner" size={20} />
+      ) :  isError ? (
+        <div className="loadingSpinner">
+          <h1>Not Found 😢</h1>
+       
+        </div>
+      ) : (
+        <div className="searchContienr">
+          {queryData.length > 0 &&
+            queryData.map((news) => {
+              return (
+                <div key={news.objectID}>
+                  {news.title && (
+                    <div  onClick={() => handleDiv}>
+                      <Link to={`/infodata/${news.objectID}`}>
+                        <div
+                          style={{
+                            width: "180px",
+                            borderRadius: "10px",
+                            margin: "2rem",
+                            padding: "2rem",
+                            backgroundColor: "#8EC5FC",
+                            backgroundImage:
+                              "linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)",
+
+                            fontSize: "16px",
+                            boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+                          }}
+                         
                         >
-                            <Link to={`/infodata/${news.objectID}`}>
-                                <div
-
-                                    style={{
-                                        width: '180px',
-                                        border: '2px solid black',
-                                        margin: '2rem'
-                                    }} key={news.objectID}>
-                                    <h3>{news.title}</h3>
-
-                                </div>
-                            </Link>
+                          <h3>{news.title}</h3>
                         </div>
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </>
+  );
+};
 
-                    }
-                    </>
-                    )
-                })}
-
-            </div>}
-
-
-        </>
-    )
-}
-
-export default Home
+export default Home;
